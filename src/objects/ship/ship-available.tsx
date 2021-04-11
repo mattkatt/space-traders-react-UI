f1cb07e0874b35e41a8a9404f7370cdbb26eef46
mattkatt <m4ttk4tt@hotmail.co.uk>
2021-04-11 18:46:20 +0100

    added method to purchase available ships

--- src/objects/ship/ship-available.tsx
+++ src/objects/ship/ship-available.tsx
@@ -1,8 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { STShipAvailable } from "./ship-interface";
 import Ship, { ShipItem } from "../../components/ui/ships";
 import { Button } from "../../components/ui/buttons";
 import { CreditsHelpers } from "../../helpers";
+import { useSpaceTraderService } from "../../services";
+import { useAuth } from "../../context/auth-context";
 
 
 interface IShipAvailable {
@@ -10,22 +12,34 @@ interface IShipAvailable {
 }
 
 const ShipAvailable: FC<IShipAvailable> = ({ ship }) => {
-    const purchaseShip = (location: string) => {
-        console.log('Buy Ship: ' + location)
+    const { purchaseShip } = useSpaceTraderService()
+    const { updateUser } = useAuth()
+    const [purchasing, setPurchasing] = useState(false)
+
+    const purchase = (location: string) => {
+        if (purchasing) {
+            return
+        }
+
+        setPurchasing(true)
+
+        purchaseShip(location, ship.type).then(updatedUser => {
+            updateUser(updatedUser)
+        }).catch(error => {
+            alert(error.response.data.error.message)
+        }).finally(() => {
+            setPurchasing(false)
+        })
     }
 
     const availableLocations = () => {
         const locations = ship.purchaseLocations.map(location => {
-            const purchase = () => {
-                purchaseShip(location.location)
-            }
-
             return (
                 <tr key={ location.location }>
                     <td>{ location.location }</td>
                     <td>{ CreditsHelpers.display(location.price) }</td>
                     <td>
-                        <Button content="Buy" onClick={ purchase } />
+                        <Button content="Buy" onClick={ () => purchase(location.location) } />
                     </td>
                 </tr>
             )
