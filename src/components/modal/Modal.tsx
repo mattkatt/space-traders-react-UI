66a424a4d4f9b1a7c856b8cbd0c70794b56e55c1
mattkatt <m4ttk4tt@hotmail.co.uk>
2021-04-13 00:25:57 +0100

    added simple modal

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,72 @@
+import React, { FC, MouseEvent, ReactElement, useEffect, useState } from "react";
+import styles from "./modal.module.css"
+
+interface IModal {
+    title?: string
+    content?: ReactElement
+    footer?: ReactElement
+    display?: boolean,
+    onDismiss?: () => void
+}
+
+const Modal: FC<IModal> = ({
+    title,
+    content,
+    children,
+    footer,
+    display,
+    onDismiss
+}) => {
+    const [modalState, setModalState] = useState(false)
+
+    useEffect(() => {
+        if (display) {
+            setModalState(display)
+        }
+    }, [display])
+
+
+    const dismissModal = () => {
+        setModalState(false)
+
+        if (onDismiss) {
+            onDismiss()
+        }
+    }
+
+    const clickOverlay = (event: MouseEvent) => {
+        event.preventDefault()
+
+        if (event.target=== event.currentTarget) {
+            dismissModal()
+        }
+    }
+
+    const modalTitle = !title ? null : (
+        <h3 className={ styles.modalTitle }>
+            { title }
+        </h3>
+    )
+
+    const modalFooter = !footer ? null : (
+        <div className={ styles.modalFooter }>
+            { footer }
+        </div>
+    )
+
+    return !modalState ? null : (
+        <div className={ styles.modalOverlay } onClick={ clickOverlay }>
+            <figure className={ styles.modal }>
+                { modalTitle }
+
+                <div className={ styles.modalBody }>
+                    { content ?? children }
+                </div>
+
+                { modalFooter }
+            </figure>
+        </div>
+    )
+}
+
+export default Modal
